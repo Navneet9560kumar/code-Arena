f92c1fe8e7b0538d4f7aa8bcddc56858bb28a0fe
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-04-01 09:37:41 +0530

    add auth

--- backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import {login,signup,logout,verify} from "../controllers/authController.js"
-const authRouter=express.Router();
-authRouter.post("/signup",signup);
-authRouter.post("/login",login);
-authRouter.get("/logout",logout);
-authRouter.get("/verify",verify);
-export default authRouter;
